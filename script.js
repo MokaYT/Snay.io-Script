c7b97df114d9e5798659ae76bf7c4c21cd45877a
Moka YT <141146299+MokaYT@users.noreply.github.com>
2024-01-06 19:51:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,78 @@
-console.log("I am on" + isMobile() == true ? "Mobile" : "PC");
+window.loggedIn = false;
+const isMobile = () => (window.innerWidth < 800) && ("ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0);
+window.onMobile = isMobile();
+
+console.log(`I am on ${isMobile() ? 'Mobile' : 'PC'}`);
 
 	function setupUI(){
 		var style = document.createElement('style');
     style.type = 'text/css';
 
     var css = `
-        #modmenu {
-            background-color: #282b30;
-            border: none;
-            border-radius: 10px;
-            margin-top: 100px;
-            margin-right: -40px;
-            padding-left: 10px;
-        }
+    #modmenubtn {
+  z-index: 21;
+  top: 30vh;
+  width: 10vh;
+  height: 10vh;
+  border-style: none;
+  background: url('https://imgur.com/QygCc3e.png');
+  background-size: 10vh 10vh;
+  background-repeat: no-repeat;
+  position: fixed;
+  right: 5vh;
+  opacity: 1;
+}
 
-#modmenubtn {
-      z-index: 21;
-      top: 20vh;
-      width: 10vh;
-      height: 10vh;
-      border-style: none;
-      background: url('https://imgur.com/h32qVjT.png');
-      background-size: 10vh 10vh;
-      background-repeat: no-repeat;
-      position: fixed;
-      right: 3vh;
-      opacity: 1
-      }
+#modmenuclosebtn {
+  z-index: 21;
+  top: 1vh;
+  width: 6vmin;
+  height: 6vmin;
+  border-style: none;
+  background: url('https://imgur.com/x9VGklK.png');
+  background-size: 6vmin;
+  background-repeat: no-repeat;
+  position: fixed;
+  right: 6vh;
+}
 
-        #modmenuclosebtn {
-            scale: 1;
-            margin-top: 84px;
-            margin-right: 117px;
-            background: url("https://imgur.com/IrrCXht.png");
-			      background-size: contain;
-			      position: absolute;
-			      left: -3%;
-			      top: -14%;
+#modmenu {
+  z-index: 20;
+  top: 33vh;
+  width: 30vmin;
+  height: 40vmin;
+  border-style: solid;
+  border-radius: 20px;
+  border-color: #353634;
+  background-color: rgb(0 0 0 / 60%);
+  position: absolute;
+  right: 6vh;
+}
 
+#modmenu input {
+  width: 85%;
+}
 
-        }
+#modmenutitle {
+  font-size: 2.5vmin;
+  margin: 0;
+  position: relative;
+  display: block;
+  height: 3vmin;
+  text-align: center;
+  color: #8c8282;
+  z-index: -1;
+}
+
+.modmenusubtitle {
+  font-size: 2vmin;
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 1vmin;
+  text-align: center;
+  color: #b2acac;
+}
 		#title {
 		 z-index: 7;
 		}
@@ -83,7 +116,185 @@ console.log("I am on" + isMobile() == true ? "Mobile" : "PC");
     align-items: center;
     text-align: center;
     width: 33vmin;
-    height: 24vmin
+    height: 24vmin;
+
+    #gallery-btn {
+  border-radius: 100%;
+  width: 130px;
+  height: 130px;
+  margin: 0;
+  border-style: none;
+  z-index: 25;
+  background-color: rgba(255, 255, 255, 0.6);
+  background-image: url("");
+}
+
+#play-btn {
+  background-image: url('https://imgur.com/5CVgh9Z.png');
+  justify-content: center;
+  background-size: 90px;
+  background-repeat: no-repeat;
+  background-color: black;
+}
+
+#spectate-btn {
+  background-image: url('https://imgur.com/9uVyMAA.png');
+  justify-content: center;
+  background-size: 90px;
+  background-repeat: no-repeat;
+  background-color: black;
+}
+
+#settings-btn {
+  background-image: url('https://imgur.com/khD251j.png');
+  justify-content: center;
+  background-size: 90px;
+  background-repeat: no-repeat;
+  background-color: black;
+}
+
+#lead-btn {
+  background: url('https://imgur.com/35RVcRC.png');
+  right: 1vmin;
+}
+
+#leaveBtn {
+  opacity: 1;
+  left: 1vmin;
+  background: url('https://imgur.com/uPAWVDq.png');
+}
+
+#fullScreenBtn {
+  position: absolute;
+  width: 6vw;
+  height: 6vw;
+  top: 12vh;
+  left: 12vh;
+  opacity: 1;
+  z-index: 99;
+  background: url('https://imgur.com/j54O1rq.png');
+  background-size: 100% 100%;
+  border-style: none;
+}
+
+.nipple .back {
+  background-image: url('https://imgur.com/hUcx4VA.png');
+}
+
+.nipple .front {
+  background-image: url('https://imgur.com/XrgEyY5.png');
+}
+
+#leaderboard {
+  padding-left: 3vmin;
+  padding-right: 3vmin;
+  padding-bottom: 3vmin;
+  width: 25vmin;
+  height: 30vmin;
+  margin: 0;
+  overflow: hidden;
+  font-family: sans-serif;
+  font-weight: bold;
+  position: absolute;
+  top: 1vh;
+  right: 5vmin;
+  z-index: 1;
+  color: #eee;
+  background-image: url('https://imgur.com/6X7gjhu.png');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+
+#login {
+    width: 280px
+}
+
+#login form span {
+    background-color: #363b41;
+    border-radius: 3px 0 0 3px;
+    color: #606468;
+    display: block;
+    float: left;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    width: 50px
+}
+
+#login form input {
+    height: 50px
+}
+
+#login form input[type=email],input[type=password],input[type=text] {
+    background-color: #3b4148;
+    border-radius: 0 3px 3px 0;
+    color: #606468;
+    margin-bottom: 1em;
+    padding: 0 16px;
+    width: 230px
+}
+
+#login form input[type=submit] {
+    background-color: #302e2f;
+    border-radius: 3px;
+    -moz-border-radius: 3px;
+    -webkit-border-radius: 80px;
+    color: #fff;
+    font-weight: 700;
+    margin-bottom: 10px;
+    text-transform: uppercase;
+    width: 280px
+}
+
+#login form input[type=submit]:hover {
+    background-color: #514b4d
+}
+
+#login>p {
+    text-align: center
+}
+
+#login>p span {
+    padding-left: 5px
+}
+
+#chat_textbox {
+  margin: 0;
+  padding: 1vmin 3vmax 1vmin 3vmax;
+  border-radius: 3vmax;
+  transition: all .5s ease-in-out;
+  background: rgb(158 158 167 / 50%);
+  border: 0;
+  color: white;
+  text-indent: 2vmin;
+  font-family: "Ubuntu";
+  width: 25vmin;
+}
+
+#chat_textbox:focus {
+  background: rgb(55 52 52 / 50%);
+}
+
+#chatBtn {
+  opacity: 1;
+  background: url(./assets/img/Chat.png);
+  width: 3vmax;
+  height: 3vmax;
+  background-size: 100% 100%;
+  border-style: none;
+}
+
+#emojiBtn {
+  opacity: 1;
+  color: white;
+  background: url(./assets/img/Emoji.png);
+  width: 3vmax;
+  height: 3vmax;
+  background-size: 100% 100%;
+  border-style: none;
+}
+
+
     `;
 
     if (style.styleSheet) {
@@ -109,44 +320,17 @@ modmenu.appendChild(document.createElement("br"));
 modmenu.appendChild(document.createElement("br"));
     modmenu.appendChild(buttonsContainer);
 
-		const logo = document.createElement("img");
-		const node = document.createElement("p");
 		const menuTitle = document.querySelector("#title");
-		menuTitle.src = "https://imgur.com/hwTdZHP.png";
-		menuTitle.style.position = "absolute";
-		menuTitle.style.top = "-5%";
-		menuTitle.style.zIndex = "7";
-		/*
-		setInterval(()=>{
-			var labels = document.querySelector("#gallery-body").querySelectorAll("label");
-				for (let i = 0; i<labels.length; i++){
-					if (labels[i].nextElementSibling.checkVisibility()){
-						labels[i].style.backgroundColor = "lightblue";
-						labels[i].style.border = "3px solid blue";
-					}else {
-						labels[i].style.backgroundColor = "gray";
-						labels[i].style.border = "";
-					}
-				}
-			});
-*/
+			menuTitle.src = "https://imgur.com/z9xsEpx.png";
+			menuTitle.style = "position: absolute; top: -8%; z-index: 7; width: 45vh; height: 45vh;";
+		
 			document.querySelector("#seasons-body").style.backgroundColor = "black";
 			document.querySelector("#seasons-header").style.backgroundColor = "black";
 			document.querySelector("#store-body").style.backgroundColor = "black";
 			document.querySelector("#store-header").style.backgroundColor = "black";
 			document.querySelector("#gallery-content").style.background = "black";
 			document.querySelector("#gallery-header").style.backgroundColor = "black";
-			/*
-			var setLabels = setInterval(()=>{
-				if (document.querySelector("#gallery-content").checkVisibility()){
-				document.querySelector("#gallery-content").querySelectorAll("label").forEach((elem)=>{
-				  elem.style.backgroundColor = "gray";
-				  elem.style.borderRadius = "20%";
-				})
-					clearInterval(setLabels);
-				}
-			});
-*/
+
 			document.querySelector("#gallery-body").style.backgroundImage = "linear-gradient(to bottom, black, white, black)";
 			document.querySelector(".windowclosebtn").style.filter = "hue-rotate(200deg)";
 			document.querySelector("#settings-body").style.backgroundImage = "linear-gradient(to bottom, black, white, black)";
